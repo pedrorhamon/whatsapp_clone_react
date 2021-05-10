b033b11e167a89ab46c942fc66ee0dd78f59db09
Pedro <pedrorhamon16@gmail.com>
2021-05-10 11:15:48 -0300

    incrementando e estilizando o sidebar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,20 @@ export default  () => {
   return (
     <div className="app-window">
       <div className="sidebar">
-      ----
+
+          <header>
+            <img className="header-avatar" src="https://image.freepik.com/vetores-gratis/perfil-de-avatar-de-homem-no-icone-redondo_24640-14044.jpg" alt=""/>
+            <div className="header-buttons">
+            </div>
+          </header>
+
+          <div className="search">
+            -----
+          </div>
+
+          <div className= "chatlist">
+            -----
+          </div>
     </div>
     <div className="contentarea">
       ----
