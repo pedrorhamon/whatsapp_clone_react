ae93b55b6af3d1da5665cd97e02e68c6bfe5c59b
Pedro <pedrorhamon16@gmail.com>
2021-05-12 10:39:55 -0300

    chatWindow-header completo part 2

--- src/components/chatWindow.js
+++ src/components/chatWindow.js
@@ -3,33 +3,63 @@ import './chatWindow.css';
 import SearchIcon from '@material-ui/icons/Search';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import CloseIcon from '@material-ui/icons/Close';
+import SendIcon from '@material-ui/icons/Send';
+import MicIcon from '@material-ui/icons/Mic';
 
 export default () => {
     return (
-       <div className="chatWindow">
-          <div className="chatWindow-header">
-              <div className = "chatWindow-headerinfo">
-                  <img className="chatWindow-avatar" src="https://image.freepik.com/vetores-gratis/perfil-de-avatar-de-homem-no-icone-redondo_24640-14044.jpg" alt=""/>
-                  <div className="chatWindow-name">Pedro</div>
-              </div>
+        <div className="chatWindow">
+            <div className="chatWindow-header">
+                <div className="chatWindow-headerinfo">
+                    <img className="chatWindow-avatar" src="https://image.freepik.com/vetores-gratis/perfil-de-avatar-de-homem-no-icone-redondo_24640-14044.jpg" alt="" />
+                    <div className="chatWindow-name">Pedro</div>
+                </div>
 
-              <div className="chatWindow-headerbuttons">
+                <div className="chatWindow-headerbuttons">
+
+                    <div className="chatWindow-btn">
+                        <SearchIcon style={{ color: '#919191' }} />
+                    </div>
+                    <div className="chatWindow-btn">
+                        <AttachFileIcon style={{ color: '#919191' }} />
+                    </div>
+                    <div className="chatWindow-btn">
+                        <MoreVertIcon style={{ color: '#919191' }} />
+                    </div>
+                </div>
+
+            </div>
+            <div className="chatWindow-body">
+
+            </div>
+
+            <div className="chatWindow-footer">
+
+                <div className="chatWindow-pre">
 
                 <div className="chatWindow-btn">
-                    <SearchIcon style={{color: '#919191'}}/>
-                </div>
-                <div className="chatWindow-btn">
-                    <AttachFileIcon style={{color: '#919191'}}/>
-                </div>
-                <div className="chatWindow-btn">
-                    <MoreVertIcon style={{color: '#919191'}}/>
-                </div>
-              </div>
+                        <InsertEmoticonIcon style={{ color: '#919191' }} />
+                    </div>
 
-              </div> 
-          <div className="chatWindow-body"></div> 
-          
-          <div className="chatWindow-footer"></div> 
-       </div>
+                </div>
+                <div className="chatWindow-inputarea">
+                    <input
+                     className="chatWindow-input" 
+                     type= "text"
+                     placeholder="Digite uma mensagem"
+                     />
+
+                </div>      
+                <div className="chatWindow-pos">
+                <div className="chatWindow-btn">
+                        <SendIcon style={{ color: '#919191' }} />
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
     );
 }
